1152c3c4039e972a0966ec2a0a7a3257a5d1a282
Yevhen Beshkarov <ybeshkarov@thingsboard.io>
2024-12-20 10:57:29 +0200

    Unit tests fixed.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,23 +6,92 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider_platform_interface/path_provider_platform_interface.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:thingsboard_app/locator.dart';
 import 'package:thingsboard_app/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const ThingsboardApp());
+const kTemporaryPath = 'temporaryPath';
+const kApplicationSupportPath = 'applicationSupportPath';
+const kDownloadsPath = 'downloadsPath';
+const kLibraryPath = 'libraryPath';
+const kApplicationDocumentsPath = 'applicationDocumentsPath';
+const kExternalCachePath = 'externalCachePath';
+const kExternalStoragePath = 'externalStoragePath';
 
-    // Verify that our counter starts at 0.
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    FlutterSecureStorage.setMockInitialValues({});
+    PathProviderPlatform.instance = FakePathProviderPlatform();
+
+    await Hive.initFlutter();
+    await setUpRootDependencies();
+  });
+
+  testWidgets('ThingsboardApp base test', (tester) async {
+    // Build our app and trigger a frame.
+    await tester.runAsync(() => tester.pumpWidget(const ThingsboardApp()));
+
     expect(
       find.byWidgetPredicate((widget) {
         if (widget is MaterialApp) {
-          return widget.title == 'ThingsBoard';
+          return widget.title == 'ThingsBoard CE App';
         }
+
         return false;
       }),
       findsOneWidget,
     );
   });
+}
+
+class FakePathProviderPlatform extends Fake
+    with MockPlatformInterfaceMixin
+    implements PathProviderPlatform {
+  @override
+  Future<String?> getTemporaryPath() async {
+    return kTemporaryPath;
+  }
+
+  @override
+  Future<String?> getApplicationSupportPath() async {
+    return kApplicationSupportPath;
+  }
+
+  @override
+  Future<String?> getLibraryPath() async {
+    return kLibraryPath;
+  }
+
+  @override
+  Future<String?> getApplicationDocumentsPath() async {
+    return kApplicationDocumentsPath;
+  }
+
+  @override
+  Future<String?> getExternalStoragePath() async {
+    return kExternalStoragePath;
+  }
+
+  @override
+  Future<List<String>?> getExternalCachePaths() async {
+    return <String>[kExternalCachePath];
+  }
+
+  @override
+  Future<List<String>?> getExternalStoragePaths({
+    StorageDirectory? type,
+  }) async {
+    return <String>[kExternalStoragePath];
+  }
+
+  @override
+  Future<String?> getDownloadsPath() async {
+    return kDownloadsPath;
+  }
 }
